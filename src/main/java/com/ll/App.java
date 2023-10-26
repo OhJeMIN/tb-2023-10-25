a60173c406a95f1aa0da978b10a47b1967769cad
jasonoh22 <jasonoh22@naver.com>
2023-10-26 14:21:35 +0900

    refactoring, divide

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,40 +5,55 @@ import java.util.List;
 import java.util.Scanner;
 
 class App {
+    Scanner scanner;
+    int lastQuotationId;
+    List<Quotation> quotations;
+
+    App() {
+        scanner = new Scanner(System.in);
+        lastQuotationId = 0;
+        quotations = new ArrayList<>();
+    }
+
     void run() {
         System.out.println("== 명언 앱 ==");
-        int lastQuotationId = 0;
-        List<Quotation> quotations = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("명령) ");
-            Scanner scanner = new Scanner(System.in);
+
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                actionWirte();
+            } else if (cmd.equals("목록")) {
+                actionList();
             }
-            else if (cmd.equals("등록")) {
-                lastQuotationId++;
+        }
+    }
 
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
+    void actionWirte() {
+        lastQuotationId++;
 
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
 
-                int id = lastQuotationId;
-                Quotation quotation = new Quotation(id, content,author);
-                quotations.add(quotation);
-                System.out.println(lastQuotationId+"번 명언이 등록되었습니다.");
-            }
-            else if (cmd.equals("목록")){
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-                if(quotations.isEmpty()) System.out.println("등록된 명언이 없습니다.");
-                for(int i=quotations.size()-1;i>=0;i--){
-                    Quotation quotation = quotations.get(i);
-                    System.out.println(quotation.id +"/"+ quotation.author+"/"+quotation.content);
-                }
-            }
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        int id = lastQuotationId;
+        Quotation quotation = new Quotation(id, content, author);
+        quotations.add(quotation);
+        System.out.println(lastQuotationId + "번 명언이 등록되었습니다.");
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        if (quotations.isEmpty()) System.out.println("등록된 명언이 없습니다.");
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+            Quotation quotation = quotations.get(i);
+            System.out.println(quotation.id + "/" + quotation.author + "/" + quotation.content);
         }
     }
 }
