dd18bc1ec5e571ad2ec97acc4ae939f82f9f22af
jasonoh22 <jasonoh22@naver.com>
2023-10-26 11:25:00 +0900

    3단계 등록 ë©함수 생성

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
+        int lastQuotationId = 0;
         while (true) {
             System.out.print("명령) ");
             Scanner scanner = new Scanner(System.in);
@@ -13,13 +14,8 @@ class App {
                 break;
             }
             else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-
-                System.out.println("1번 명언이 등록되었습니다.");
+                lastQuotationId++;
+                new registration().register(lastQuotationId);
             }
         }
     }
