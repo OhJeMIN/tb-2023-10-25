b6ee5ce058418c19281b4afc51f68468b0f88020
jasonoh22 <jasonoh22@naver.com>
2023-10-27 16:58:31 +0900

    loombook

--- src/main/java/com/ll/domain/Quotation.java
+++ src/main/java/com/ll/domain/Quotation.java
@@ -1,33 +1,16 @@
 package com.ll.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@AllArgsConstructor // 모든 필드를 입력 받는 생성자일시 사용
 public class Quotation {
+    @Getter
     private int id;
+    @Getter
+    @Setter
     private String content;
+    @Getter
+    @Setter
     private String authorName;
-
-    public Quotation(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.authorName = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
 }
